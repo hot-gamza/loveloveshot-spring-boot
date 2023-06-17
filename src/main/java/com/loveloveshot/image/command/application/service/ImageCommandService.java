47fa5f5e6e333b14da1f438cd2d7fd825a2b1ba4
manje cho <whakswp123@naver.com>
2023-06-17 13:13:22 +0900

    [#32]refactoring: 일반 이미지 업로드 리팩토링

--- src/main/java/com/loveloveshot/image/command/application/service/ImageCommandService.java
+++ src/main/java/com/loveloveshot/image/command/application/service/ImageCommandService.java
@@ -18,40 +18,6 @@ public class ImageCommandService {
     private final ImageCommandDomainService imageCommandDomainService;
     private final ImageCommandRepository imageCommandRepository;
 
-    public UploadResponse createAISingleImage(Long userNo, ImageRequest imageRequest) {
-
-        UploadResponse aiImageDTO = imageCommandDomainService.uploadStandardImage(imageRequest);
-
-//        String filePath = "C:\\AIImages/";
-//
-//        File dir = new File(filePath);
-//        if (!dir.exists()) {
-//            dir.mkdirs();   //폴더 없을 시 자동으로 하위폴더 생성
-//        }
-//
-//        String originFileName = aiImageDTO.getAiImage().getOriginalFilename();  //원본 파일 이름
-//        String ext = originFileName.substring(originFileName.lastIndexOf(".") + 1); //파일 확장자
-//        String savedName = UUID.randomUUID().toString().replaceAll("-", "") + "." + ext; //저장되는 이름
-//
-//        try {
-//            aiImageDTO.getAiImage().transferTo(new File(filePath + savedName));
-//
-//            Image image = Image.builder()
-//                    .originImageName(originFileName)
-//                    .savedImageName(savedName)
-//                    .imagePath(filePath)
-//                    .userVO(new UserVO(userNo))
-//                    .build();
-//
-//            imageCommandRepository.save(image);
-//
-//        } catch (IOException e) {
-//            new File(filePath + savedName).delete();  //업로드 후 DB저장 중 오류났을 때 업로드된 이미지 삭제해줌
-//        }
-
-        return aiImageDTO;
-    }
-
     // 일반 이미지 업로드
     public UploadResponse uploadStandardImage(ImageRequest imageRequest) {
         return imageCommandDomainService.uploadStandardImage(imageRequest);
