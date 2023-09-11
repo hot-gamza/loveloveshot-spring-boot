500b9cd75a2c0e1223f98fc3510a913b289722a1
hodin <hodin030@gmail.com>
2023-09-11 18:06:08 +0900

    [#8] feat: image 업로드 컨트롤러 작성

--- src/main/java/com/loveloveshot/image/command/application/controller/ImageCommandController.java
+++ src/main/java/com/loveloveshot/image/command/application/controller/ImageCommandController.java
@@ -1,20 +1,42 @@
 package com.loveloveshot.image.command.application.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.loveloveshot.image.command.application.dto.RequestImageListDTO;
+import com.loveloveshot.image.command.application.dto.RequestSingleImageDTO;
+import com.loveloveshot.image.command.application.service.ImageCommandService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class ImageCommandController {
 
-    @PostMapping("/images")   // 필기. RequestParam에 name값을 지정해주지 않으면 디폴트로 변수명을 key값으로 가짐.
-    public void uploadImage(@RequestParam List<MultipartFile> imageFileList) {
+    private final ImageCommandService imageCommandService;
 
+    @PostMapping("/singleImage") // 필기. RequestParam에 name값을 지정해주지 않으면 디폴트로 변수명을 key값으로 가짐.
+    public void uploadSingleImage(@RequestParam MultipartFile maleSingleImage,
+                                  @RequestParam MultipartFile femaleSingleImage,
+                                  RequestSingleImageDTO singleImageDTO) {
 
+        singleImageDTO.setMaleSingleImage(maleSingleImage);
+        singleImageDTO.setFemaleSingleImage(femaleSingleImage);
+
+        imageCommandService.getSingleImage(singleImageDTO);
     }
+
+
+    @PostMapping("/imageList")
+    public void uploadImageList(@RequestParam List<MultipartFile> maleImageList,
+                                @RequestParam List<MultipartFile> femaleImageList,
+                                RequestImageListDTO imageListDTO) {
+
+        imageListDTO.setMaleImageList(maleImageList);
+        imageListDTO.setFemaleImageList(femaleImageList);
+
+        imageCommandService.getImageList(imageListDTO);
+    }
+
 }
