ab1dd49d1adac0109cd5aaaa0a218518f2bf1854
manje cho <whakswp123@naver.com>
2023-09-12 08:35:09 +0900

    [#4]setting: ApiResponse 세팅

---
+++ src/main/java/com/loveloveshot/common/response/ApiResponse.java
@@ -0,0 +1,9 @@
+package com.loveloveshot.common.response;
+
+import com.loveloveshot.common.response.enumtype.ApiStatus;
+
+public class ApiResponse {
+    private ApiStatus status;
+    private String message;
+    private Object data;
+}
