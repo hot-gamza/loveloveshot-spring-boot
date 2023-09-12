83eb269378ef23bcdf63c5c4c6a4d6a87cd8d368
manje cho <whakswp123@naver.com>
2023-09-12 14:29:59 +0900

    [#4]setting: ApiResponse 세팅

--- src/main/java/com/loveloveshot/common/response/ApiResponse.java
+++ src/main/java/com/loveloveshot/common/response/ApiResponse.java
@@ -1,9 +1,23 @@
 package com.loveloveshot.common.response;
 
 import com.loveloveshot.common.response.enumtype.ApiStatus;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
 public class ApiResponse {
     private ApiStatus status;
     private String message;
     private Object data;
+
+    public static ApiResponse success(String message, Object data) {
+        return new ApiResponse(ApiStatus.SUCCESS, message, data);
+    }
+
+    public static ApiResponse error(String message) {
+        return new ApiResponse(ApiStatus.ERROR, message, null);
+    }
 }
