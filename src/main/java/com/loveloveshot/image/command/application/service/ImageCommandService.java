b9c9367fdd93366faa99ed980e244aecade49736
hodin030 <hodin030@gmail.com>
2023-09-13 06:54:47 +0900

    feat: 서비스 작성

--- src/main/java/com/loveloveshot/image/command/application/service/ImageCommandService.java
+++ src/main/java/com/loveloveshot/image/command/application/service/ImageCommandService.java
@@ -2,23 +2,62 @@ package com.loveloveshot.image.command.application.service;
 
 import com.loveloveshot.image.command.application.dto.RequestImageListDTO;
 import com.loveloveshot.image.command.application.dto.RequestSingleImageDTO;
+import com.loveloveshot.image.command.application.dto.SaveImageDTO;
+import com.loveloveshot.image.command.domain.aggregate.entity.Image;
+import com.loveloveshot.image.command.domain.aggregate.vo.UserVO;
+import com.loveloveshot.image.command.domain.repository.ImageCommandRepository;
 import com.loveloveshot.image.command.domain.service.ImageCommandDomainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ImageCommandService {
 
     private final ImageCommandDomainService imageCommandDomainService;
+    private final ImageCommandRepository imageCommandRepository;
 
-    public void getSingleImage(RequestSingleImageDTO singleImageDTO) {
+    public void transferSingleImage(RequestSingleImageDTO singleImageDTO) {
 
-        imageCommandDomainService.createAISingleImage(singleImageDTO);
+        imageCommandDomainService.getAISingleImage(singleImageDTO);
     }
 
-    public void getImageList(RequestImageListDTO imageListDTO) {
+    public void transferImageList(RequestImageListDTO imageListDTO) {
 
-        imageCommandDomainService.createAIImageList(imageListDTO);
+        imageCommandDomainService.getAIImageList(imageListDTO);
+    }
+
+    public void createAISingleImage(MultipartFile aiImage, Long userNO) {
+
+        String root = "C:\\app-file";
+        String filePath = root + "/AIImages";
+
+        File dir = new File(filePath);
+        if (!dir.exists()) {
+            dir.mkdirs();   //폴더 없을 시 자동으로 하위폴더 생성
+        }
+
+        String originFileName = aiImage.getOriginalFilename();  //원본 파일 이름
+        String ext = originFileName.substring(originFileName.lastIndexOf(".") + 1); //파일 확장자
+        String savedName = UUID.randomUUID().toString().replaceAll("-", "") + "." + ext; //저장되는 이름
+
+        try {
+            aiImage.transferTo(new File(filePath + savedName));
+            Image image = Image.builder()
+                    .originImageName(originFileName)
+                    .savedImageName(savedName)
+                    .imagePath(filePath)
+                    .userVO(new UserVO(userNO))
+                    .build();
+
+        } catch (IOException e) {
+            new File(filePath + savedName).delete();  //업로드 후 DB저장 중 오류났을 때 업로드된 이미지 삭제해줌
+
+        }
     }
 }
