9a940699c597b74f11955b8afbabd69d1e8f0440
manje cho <whakswp123@naver.com>
2023-09-13 19:44:55 +0900

    [#18]feat: Logging 테스트 케이스 작성

--- src/test/java/com/loveloveshot/LoggingTest.java
+++ src/test/java/com/loveloveshot/LoggingTest.java
@@ -1,5 +1,6 @@
 package com.loveloveshot;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ public class LoggingTest {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Test
+    @DisplayName("Logging 테스트 : success")
     public void log4j2LoggerTest() {
         System.out.println("안녕 난 println이야");
         logger.info("안녕 난 logger야");
