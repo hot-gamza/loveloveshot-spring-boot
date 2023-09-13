f04fcceeb84930b31d54b9dcb9edb065acc8bbb3
hodin030 <134986912+hodin030@users.noreply.github.com>
2023-09-14 00:24:36 +0900

    [Feature] - {IMAGE} 이미지 컨트롤러 및 서비스 기능 추가 (#22)
    * feat: image 엔티티 생성
    
    * feat: 이미지 업로드 페이지
    
    * feat: 이미지 업로드 컨트롤러
    
    * [#8] feat: image 업로드 컨트롤러 작성
    
    * feat: image 업로드 RequestDTO 작성
    
    * [#10] feat: image 업로드 서비스 코드 작성
    
    * feat: image 업로드 페이지 작성
    
    * feat: image 업로드 컨트롤러 작성
    
    * rename: 메소드명 수정
    
    * feat: 엔티티 수정 및 VO 생성
    
    * feat: 레포지토리 생성
    
    * feat: 컨트롤러 수정
    
    * feat: 서비스 작성
    
    * setting: gitignore 추가
    
    * feat: UserVO 생성
    
    * feat: 레포지토리 생성
    
    * feat: 이미지 저장 기능 추가
    
    * feat: 이미지 업로드 및 생성된 AI 이미지 불러오기 기능
    
    * Refactor: 사용하지 않는 기능 주석 처리
    
    * Refactor: 테스트코드 주석 해제 및 서비스 어노테이션 수정

--- src/main/java/com/loveloveshot/image/command/domain/service/ImageCommandDomainService.java
+++ src/main/java/com/loveloveshot/image/command/domain/service/ImageCommandDomainService.java
@@ -1,13 +1,15 @@
 package com.loveloveshot.image.command.domain.service;
 
-import com.loveloveshot.image.command.application.dto.RequestImageListDTO;
-import com.loveloveshot.image.command.application.dto.RequestSingleImageDTO;
+import com.loveloveshot.common.annotation.DomainService;
+import com.loveloveshot.image.command.application.dto.ImageListRequestDTO;
+import com.loveloveshot.image.command.application.dto.SingleImageRequestDTO;
+import com.loveloveshot.image.command.application.dto.AIImageResponseDTO;
 import org.springframework.stereotype.Service;
 
-@Service
+@DomainService
 public interface ImageCommandDomainService {
 
-    void getAISingleImage(RequestSingleImageDTO singleImageDTO);
+    AIImageResponseDTO getAISingleImage(SingleImageRequestDTO singleImageDTO);
 
-    void getAIImageList(RequestImageListDTO imageListDTO);
+//    void getAIImageList(ImageListRequestDTO imageListDTO);
 }
