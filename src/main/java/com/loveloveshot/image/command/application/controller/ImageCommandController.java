9e44a482496192429ea1c4d81f7b351a86e31ca0
아맞다우산 <98208452+fakerdeft@users.noreply.github.com>
2023-09-15 17:41:30 +0900

    [FEATURE] - {Image} Infra Service 구현 (#26)
    * [#18]setting: Log4j2 세팅
    
    * [#18]feat: Logging 테스트 케이스 작성
    
    * [#18]feat: Logging 테스트 케이스 작성
    
    * [#23]refactoring: 안 쓰는 테스트 주석 처리
    
    * [#23]feat: AI 단독 이미지 생성 API 연결 메소드
    
    * [#23]feat: infra service 작성
    
    * [#23]feat: AI 단독 이미지 생성 API 연결 구현
    
    * [#23]feat: AI 단독 이미지 생성 API 연결 구현

--- src/main/java/com/loveloveshot/image/command/application/controller/ImageCommandController.java
+++ src/main/java/com/loveloveshot/image/command/application/controller/ImageCommandController.java
@@ -1,14 +1,19 @@
 package com.loveloveshot.image.command.application.controller;
 
 import com.loveloveshot.common.response.ApiResponse;
-import com.loveloveshot.image.command.application.dto.ImageListRequestDTO;
+import com.loveloveshot.image.command.application.dto.ImagesDTO;
 import com.loveloveshot.image.command.application.dto.SingleImageRequestDTO;
 import com.loveloveshot.image.command.application.service.ImageCommandService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -25,20 +30,49 @@ public class ImageCommandController {
         singleImageDTO.setMaleSingleImage(maleSingleImage);
         singleImageDTO.setFemaleSingleImage(femaleSingleImage);
 
-        if(maleSingleImage.isEmpty() || femaleSingleImage.isEmpty()) {
+        if (maleSingleImage.isEmpty() || femaleSingleImage.isEmpty()) {
             throw new IllegalArgumentException("사진을 첨부해주세요");
         }
 
         // 설명. 파일 확장자 이미지 형식인지 확인
-        if(!maleSingleImage.getContentType().startsWith("image") ||
+        if (!maleSingleImage.getContentType().startsWith("image") ||
                 !femaleSingleImage.getContentType().startsWith("image")) {
             throw new IllegalArgumentException("이미지 형식의 파일을 올려주세요");
         }
 
         Long userNo = 1L;   //임의 값
 
-        return ApiResponse.success("성공적으로 등록되었습니다.", imageCommandService.createAISingleImage(userNo, singleImageDTO));
+        String filePath = "C:\\originalAiImages/";
 
+        File dir = new File(filePath);
+        if (!dir.exists()) {
+            dir.mkdirs();   //폴더 없을 시 자동으로 하위폴더 생성
+        }
+
+        String originFileName1 = maleSingleImage.getOriginalFilename();  //원본 파일 이름
+        String ext1 = originFileName1.substring(originFileName1.lastIndexOf(".") + 1); //파일 확장자
+        String savedName1 = UUID.randomUUID().toString().replaceAll("-", "") + "." + ext1; //저장되는 이름
+
+        String originFileName2 = maleSingleImage.getOriginalFilename();  //원본 파일 이름
+        String ext2 = originFileName2.substring(originFileName2.lastIndexOf(".") + 1); //파일 확장자
+        String savedName2 = UUID.randomUUID().toString().replaceAll("-", "") + "." + ext2; //저장되는 이름
+
+        System.out.println("filePath = " + filePath + savedName1);
+        System.out.println("filePath = " + filePath + savedName2);
+
+        try {
+            maleSingleImage.transferTo(new File(filePath + savedName1));
+            femaleSingleImage.transferTo(new File(filePath + savedName2));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        ImagesDTO imagesDTO = new ImagesDTO();
+        imagesDTO.setMaleImage(filePath + savedName1);
+        imagesDTO.setFemaleImage(filePath + savedName2);
+
+        return ApiResponse.success("성공적으로 등록되었습니다."
+                , imageCommandService.createAISingleImage(userNo, singleImageDTO, imagesDTO));
     }
 
 
--- src/main/java/com/loveloveshot/image/command/infrastructure/service/ImageCommandInfraService.java
+++ src/main/java/com/loveloveshot/image/command/infrastructure/service/ImageCommandInfraService.java
@@ -1,32 +1,73 @@
 package com.loveloveshot.image.command.infrastructure.service;
 
 import com.loveloveshot.common.annotation.InfraService;
-import com.loveloveshot.image.command.application.dto.ImageListRequestDTO;
-import com.loveloveshot.image.command.application.dto.SingleImageRequestDTO;
 import com.loveloveshot.image.command.application.dto.AIImageResponseDTO;
+import com.loveloveshot.image.command.application.dto.ImagesDTO;
+import com.loveloveshot.image.command.application.dto.SingleImageRequestDTO;
 import com.loveloveshot.image.command.domain.service.ImageCommandDomainService;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
+import org.json.simple.JSONObject;
+
+import java.util.Map;
 
 @InfraService
 public class ImageCommandInfraService implements ImageCommandDomainService {
+    /**
+     * Map을 jsonString으로 변환
+     */
+    @SuppressWarnings("unchecked")
+    public static String getJsonStringFromMap(Map<String, Object> map) {
 
-    private MultipartFile aiImage;
+        JSONObject json = new JSONObject();
+
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+
+            String key = entry.getKey();
+            Object value = entry.getValue();
+
+            json.put(key, value);
+        }
+
+        return json.toJSONString();
+    }
 
     @Override
-    public AIImageResponseDTO getAISingleImage(SingleImageRequestDTO singleImageDTO) {
+    public AIImageResponseDTO getAISingleImage(SingleImageRequestDTO singleImageDTO, ImagesDTO imagesDTO) {
+        System.out.println("imagesDTO1 = " + imagesDTO.getMaleImage());
+        System.out.println("imagesDTO2 = " + imagesDTO.getFemaleImage());
 
-        System.out.println(singleImageDTO.getMaleSingleImage().getOriginalFilename());
+        String reqURL = "http://192.168.0.40:5001/"; // AI 단독 이미지 생성 API URL
 
-        AIImageResponseDTO AIImageResponseDTO = new AIImageResponseDTO();
+        // WebClient 방식
 
-        AIImageResponseDTO.setAiImage(aiImage);
 
-        return AIImageResponseDTO;
+//        HttpURLConnection 방식
+//
+//        try {
+//            // Set header
+//            Map<String, String> headers = new HashMap<>();
+//            HttpPostMultipart multipart = new HttpPostMultipart(reqURL, "utf-8", headers);
+//
+//            // Add form field
+////            multipart.addFormField("username", "test_name");
+////            multipart.addFormField("password", "test_psw");
+//
+//            // Add file
+//            multipart.addFilePart("imgFile", new File(imagesDTO.getMaleImage()));
+//            multipart.addFilePart("imgFile", new File(imagesDTO.getFemaleImage()));
+//
+//            // Print result
+//            String response = multipart.finish();
+//            System.out.println(response);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        return null;
     }
 
 //    @Override
 //    public void getAIImageList(ImageListRequestDTO imageListDTO) {
 //        System.out.println(imageListDTO.getMaleImageList().get(0).getOriginalFilename());
 //    }
+
 }
